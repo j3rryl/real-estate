6762383d5780360bdd407d634bdc8ef9e2f5af34
j3rryl <jeremymunroe0@gmail.com>
2024-08-27 12:35:32 +0300

    update columns

--- db/schema.ts
+++ db/schema.ts
@@ -1,14 +1,36 @@
 import { relations, sql } from "drizzle-orm";
-import { bigint, mysqlTable, timestamp, varchar } from "drizzle-orm/mysql-core";
+import {
+  bigint,
+  decimal,
+  float,
+  int,
+  mysqlEnum,
+  mysqlTable,
+  text,
+  timestamp,
+  varchar,
+} from "drizzle-orm/mysql-core";
 
 export const properties = mysqlTable("properties", {
   id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
   name: varchar("name", { length: 255 }),
+  description: text("description"),
+  price: decimal("price", { precision: 10, scale: 2 }),
+  location: varchar("location", { length: 255 }),
+  propertyType: varchar("property_type", { length: 255 }),
+  status: mysqlEnum("status", ["available", "sold", "rented"]).default(
+    "available"
+  ),
+  bedrooms: int("bedrooms"),
+  bathrooms: int("bathrooms"),
+  area: float("area"),
   thumbnail: varchar("thumbnail", { length: 255 }),
   createdAt: timestamp("createdAt", { mode: "date" })
     .notNull()
     .default(sql`now()`),
-  updatedAt: timestamp("updatedAt").defaultNow().onUpdateNow(),
+  updatedAt: timestamp("updatedAt", { mode: "date" })
+    .defaultNow()
+    .onUpdateNow(),
 });
 
 export const propertyImages = mysqlTable("property_images", {
--- db/seeder.ts
+++ db/seeder.ts
@@ -23,11 +23,29 @@ const main = async () => {
     const batch = [];
     for (let j = 0; j < batchSize && i + j < records; j++) {
       batch.push({
-        name: faker.location.city(),
+        name: faker.company.name(),
+        description: faker.commerce.productDescription(),
+        price: `${faker.number.float({
+          min: 1000,
+          max: 10_000_000,
+          fractionDigits: 2,
+        })}`,
+        location: faker.location.city(),
+        propertyType: faker.helpers.arrayElement([
+          "bungalow",
+          "apartment",
+          "house",
+        ]),
+        status: faker.helpers.arrayElement(["available", "sold", "rented"]),
+        bedrooms: faker.number.int({ min: 1, max: 10 }),
+        bathrooms: faker.number.int({ min: 1, max: 10 }),
+        area: faker.number.float({ min: 100, max: 1000 }),
         thumbnail: faker.image.url(),
       });
     }
+    // @ts-ignore
     tasks.push(db.insert(properties).values(batch));
+    // @ts-ignore
   }
   await Promise.all(tasks);
   console.timeEnd("Seeding complete");
