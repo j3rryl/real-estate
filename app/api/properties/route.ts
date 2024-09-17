ac9b3825546b93de29217474e68887413ada1e8f
j3rryl <jeremymunroe0@gmail.com>
2024-09-17 23:12:07 +0300

    query optimizations

--- app/api/properties/route.ts
+++ app/api/properties/route.ts
@@ -1,16 +1,14 @@
 import { db } from "@/db";
-import { properties } from "@/db/schema";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const page = Number(searchParams.get("page")) || 1;
   const pageSize = Number(searchParams.get("pageSize")) || 10;
-  const getProperties = await db
-    .select()
-    .from(properties)
-    .limit(pageSize)
-    .offset((page - 1) * pageSize);
+  const getProperties = await db.query.properties.findMany({
+    limit: pageSize,
+    offset: (page - 1) * pageSize,
+  });
 
   return Response.json(getProperties, { status: 200 });
 }
