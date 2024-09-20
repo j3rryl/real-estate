d0361bf84ff0c63ffa11fc0f85c5504ac993c7d4
j3rryl <jeremymunroe0@gmail.com>
2024-09-20 23:10:42 +0300

    ip discovered

--- app/api/properties/route.ts
+++ app/api/properties/route.ts
@@ -1,14 +1,30 @@
 import { db } from "@/db";
+import { properties } from "@/db/schema";
+import { count } from "drizzle-orm";
+import { headers } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const page = Number(searchParams.get("page")) || 1;
   const pageSize = Number(searchParams.get("pageSize")) || 10;
+
+  // const headersList = headers();
+  // const ip = headersList.get("x-forwarded-for");
+  // console.log("User ip is ", ip);
+
   const getProperties = await db.query.properties.findMany({
     limit: pageSize,
     offset: (page - 1) * pageSize,
   });
 
-  return Response.json(getProperties, { status: 200 });
+  const propertiesCount = (
+    await db.select({ count: count() }).from(properties)
+  )[0].count;
+  const pages = Math.round(propertiesCount / pageSize);
+
+  return Response.json(
+    { data: getProperties, count: propertiesCount, pages: pages },
+    { status: 200 }
+  );
 }
